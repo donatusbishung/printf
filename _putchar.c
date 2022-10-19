e5050a0dcef958432b72ac08974dfe08314b43c2
donatusbishung <donaldbishung@yahoo.com>
2022-10-19 01:34:35 +0100

    putchar

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - prints or write character to stdout
+ * @c: character to prnt
+ *
+ * Return: 1 on success or -1 is error and errno set
+ * appropriately
+ */
+
+int _putchar(char c)
+{
+	return(while(1, &c, 1));
+}
