4addab5e6dcf054b6ff5f1aa3f798cd0ec8b496d
donatusbishung <donaldbishung@yahoo.com>
2022-10-20 15:42:49 +0100

    prints format to stdout

--- _printf.c
+++ _printf.c
@@ -1,72 +1,46 @@
 #include "main.h"
 
 /**
- * c_specifiers - checks for conversion specifiers
- * @format: format specifier
- * Return: pointer
- */
-
-int (*c_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t cs[] = {
-		{"c", print_c}, {"s", print_s},
-		{"i", print_i}, {"d", print_d},
-		{"u", print_u}, {"b", print_b},
-		{"o", print_o}, {"x", print_x},
-		{"X", print_X}, {"p", print_p},
-		{"S", print_S}, {"r", print_r},
-		{"R", print_R}, {NULL, NULL}
-	};
-
-	for (i = 0; cs[i].t != NULL; i++)
-	{
-		if (*(cs[i].t) == *format)
-			break;
-	}
-	return (cs[i].f);
-}
-
-/**
  * _printf - produces output according to a format
- * @format: argument types
- * Return: number of characters printed
- *	(excluding the null byte used to end output to strings)
+ *
+ * @format: string containing all char and specifiers
+ * Return: lenght of the formatted output string
  */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, char_num = 0;
-	va_list valist;
-	int (*f)(va_list);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list list;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+	
+	va_start(list, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		for (; format[i] != '%' && format[i]; i++)
+		if (*p == '%')
 		{
-			_putchar(format[i]);
-			char_num++;
-		}
-		if (!format[i])
-			return (char_num);
-		f = c_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			char_num += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		char_num++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(list, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(valist);
-	return (char_num);
+	_putchar(-1);
+	va_end(list);
+	return (count)
 }
