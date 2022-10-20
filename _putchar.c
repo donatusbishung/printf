bb23fb0a47dcfd42bc9dc7bb3cac2598f59c94b9
keme-ebi <kelvin6bolou@gmail.com>
2022-10-20 10:25:26 -0700

    fix

--- _putchar.c
+++ _putchar.c
@@ -10,5 +10,32 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string pointer
+ * Return: number of char
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
